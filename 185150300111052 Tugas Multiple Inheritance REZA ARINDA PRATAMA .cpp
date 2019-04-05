0960e63bab850114d8f9f470500b1bbe47fed34b
rezaarinda <49304536+rezaarinda@users.noreply.github.com>
2019-04-05 16:24:51 +0700

    Add files via upload

---
+++ 185150300111052 Tugas Multiple Inheritance REZA ARINDA PRATAMA .cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class sepedaDewasa{
+	
+	public :
+		int jmlhrodadewasa(){
+			return 2;
+		}
+		void cetak(){
+			cout<<"Jumlah Roda : ";
+		}
+	
+};
+
+class sepedaBayi{
+		
+	public :
+		int jmlhrodabayi(){
+			return 3;
+		}
+		void cetak(){
+			cout<<"Jumlah Roda : ";
+		}
+};
+
+class sepeda: public sepedaDewasa,public sepedaBayi{
+	private :
+		string jenis;
+	public : 
+		void setJenis(string jenis){
+		this->jenis=jenis;
+		}
+};
+
+int main(){
+	string jenis;
+	sepeda sepeda1;
+	sepedaDewasa a;
+	sepedaBayi b;
+	cout<<"Masukkan Jenis Sepeda(dewasa/bayi) : ";
+	cin>>jenis;
+	sepeda1.setJenis(jenis);
+	if(jenis=="dewasa"){
+	a.cetak();
+		cout<<sepeda1.jmlhrodadewasa();
+		}
+		else if(jenis=="bayi"){
+	b.cetak();
+			cout<<sepeda1.jmlhrodabayi();
+		}
+}
+	
+
+	
